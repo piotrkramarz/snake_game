9ac9b870018eb9a119a1867814ab9d6fd4d790fd
piotrkramarz <piotr.m.kramarz@gmail.com>
2017-11-07 18:38:49 +0100

    Add snake
    Add controls, add speed interval and score

--- game.js
+++ game.js
@@ -21,6 +21,40 @@ $(function () {
     drawSpeedUp();
     drawSlowDown();
 
+    var moves = {
+        38: function up($node) {
+            return $node.parent().prev().find('.cell').eq($node.index());
+        },
+        40: function down($node) {
+            return $node.parent().next().find('.cell').eq($node.index());
+        },
+        37: function left($node) {
+            return $node.prev();
+        },
+        39: function right($node) {
+            return $node.next();
+        }
+    };
+
+    moves[38].invalid = 40;
+    moves[40].invalid = 38;
+    moves[37].invalid = 39;
+    moves[39].invalid = 37;
+
+    var direction = moves[40];
+
+    $(window).on('keydown', function (event) {
+
+        var code = event.keyCode
+        console.log(code)
+
+        if (code === direction.invalid) {
+            return
+        }
+
+        direction = moves[event.keyCode] || direction
+    });
+
     function drawApple() {
         var $emptyCells = $('td:not(".snake")');
 
@@ -39,4 +73,53 @@ $(function () {
         $emptyCells.eq(Math.floor(Math.random() * $emptyCells.length)).addClass('slowDown')
     }
 
+    var points = 0;
+    var interval;
+    function runInterval(s){
+        var speed = s;
+        clearInterval(interval);
+        interval = setInterval(function () {
+
+            var tail = snakeCells[0];
+            var head = snakeCells[snakeCells.length - 1];
+
+            $(head).removeClass('head');
+
+            var $nextHead = direction($(head));
+
+            if ($nextHead.hasClass('snake') || $nextHead.length === 0) {
+                clearInterval(interval);
+                alert('GAME OVER');
+                return;
+            }
+
+            if ($nextHead.hasClass('slowDown') === true) {
+                $nextHead.removeClass('slowDown');
+                runInterval(1000)
+                drawSlowDown()
+            }
+
+            if ($nextHead.hasClass('speedUp') === true) {
+                $nextHead.removeClass('speedUp');
+                runInterval(300)
+                drawSpeedUp()
+            }
+
+            if ($nextHead.hasClass('apple') === false) {
+                $(tail).removeClass('snake');
+                snakeCells = snakeCells.slice(1);
+            } else {
+                $nextHead.removeClass('apple');
+                points += 1;
+                drawApple()
+            }
+
+            $('.score').text(points);
+
+            snakeCells = snakeCells.concat($nextHead.get(0));
+            $nextHead.addClass('head snake');
+
+        }, speed)
+    }
+    runInterval(500)
 });
